1978b9b8cab5576082cef3bc30240d85b04c3418
Andri Yudianto <andriyudianto60@gmail.com>
2020-10-15 23:27:03 +0700

    add position

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const PORT = process.env.PORT || 3000
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+const express = require("express")();
+const http = require("http").Server(express);
+const socketio = require("socket.io")(http);
 
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
-app.use(express.json())
-
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-})
-
-
-http.listen(PORT, () => {
-    console.log('run :'+PORT)
-})
-
-
+let position = {
+    x: 200,
+    y: 200
+  };
+  
+  socketio.on("connection", socket => {
+    socket.emit("position", position);
+    socket.on("move", direction => {
+      switch (direction) {
+        case "left":
+          position.x -= 5;
+          socketio.emit("position", position);
+          break;
+        case "right":
+          position.x += 5;
+          socketio.emit("position", position);
+          break;
+        case "up":
+          position.y -= 5;
+          socketio.emit("position", position);
+          break;
+        case "down":
+          position.y += 5;
+          socketio.emit("position", position);
+          break;
+      }
+    });
+  });
+  
+  http.listen(3000, () => {
+    console.log("Listening at port 3000...");
+  });
