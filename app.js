e75acb5b8145fc3fbeca7b2a5ad8d6156bf962e7
PutraRezaFardani <eternal.putra@gmail.com>
2020-10-15 23:41:25 +0700

    ok

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-const express = require("express")();
-const http = require("http").Server(express);
-const socketio = require("socket.io")(http);
+const app = require("express")();
+const server = require("http").Server(app);
+const socketio = require("socket.io")(server);
 
 let position = {
     x: 200,
     y: 200
   };
   
-  socketio.on("connection", socket => {
+  io.on("connection", socket => {
     socket.emit("position", position);
     socket.on("move", direction => {
       switch (direction) {
         case "left":
           position.x -= 5;
-          socketio.emit("position", position);
+          io.emit("position", position);
           break;
         case "right":
           position.x += 5;
-          socketio.emit("position", position);
+          io.emit("position", position);
           break;
         case "up":
           position.y -= 5;
-          socketio.emit("position", position);
+          io.emit("position", position);
           break;
         case "down":
           position.y += 5;
-          socketio.emit("position", position);
+          io.emit("position", position);
           break;
       }
     });
   });
   
-  http.listen(3000, () => {
+  server.listen(3000, () => {
     console.log("Listening at port 3000...");
   });
